1fc6f27a9995126c5598feb17a2f9c88ff19a7a0
James Green <james@epicrideweather.com>
2025-08-01 10:54:15 -0700

    revert removal of executor preprocessor

--- lib/src/stream/tileset_executor_preprocessor.dart
+++ lib/src/stream/tileset_executor_preprocessor.dart
@@ -2,21 +2,60 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:executor_lib/executor_lib.dart';
-import 'package:vector_map_tiles/src/stream/tileset_ui_preprocessor.dart';
 import 'package:vector_tile_renderer/vector_tile_renderer.dart';
 
 import '../../vector_map_tiles.dart';
+import '../extensions.dart';
 
 class TilesetExecutorPreprocessor {
-  late final TilesetUiPreprocessor _delegate;
+  final TilesetPreprocessor _preprocessor;
+  final Executor _executor;
+  bool _ready = false;
+  final _readyCompleter = Completer<bool>();
 
+  TilesetExecutorPreprocessor(this._preprocessor, this._executor) {
+    _initialize();
+  }
 
-  TilesetExecutorPreprocessor(TilesetPreprocessor preprocessor, Executor _) {
-    _delegate = TilesetUiPreprocessor(preprocessor);
+  void _initialize() async {
+    final futures = _executor.submitAll(Job(
+        'setup preprocessor', _setupPreprocessor, _preprocessor,
+        deduplicationKey: null));
+    for (final future in futures) {
+      await future;
+    }
+    _ready = true;
+    _readyCompleter.complete(true);
   }
 
   Future<Tileset> preprocess(TileIdentity identity, Tileset tileset,
       Rectangle<double>? clip, int zoom, CancellationCallback cancelled) async {
-    return _delegate.preprocess(identity, tileset, clip, zoom, cancelled);
+    if (!_ready) {
+      await _readyCompleter.future;
+    }
+    final deduplicationKey =
+        'preprocess: $identity clip=$clip zoom=$zoom sources=${tileset.tiles.keys.toList().sorted().join(',')}';
+    final preprocessed = await _executor.submit(Job(deduplicationKey,
+        _preprocessTile, _TilesetAndZoom(_preprocessor.theme.id, tileset, zoom),
+        cancelled: cancelled, deduplicationKey: deduplicationKey));
+    return preprocessed;
   }
 }
+
+final _preprocessorByThemeId = <String, TilesetPreprocessor>{};
+
+class _TilesetAndZoom {
+  final String themeId;
+  final Tileset tileset;
+  final int zoom;
+
+  _TilesetAndZoom(this.themeId, this.tileset, this.zoom);
+}
+
+Future<void> _setupPreprocessor(TilesetPreprocessor preprocessor) async {
+  _preprocessorByThemeId[preprocessor.theme.id] = preprocessor;
+}
+
+Tileset _preprocessTile(_TilesetAndZoom it) =>
+    _preprocessorByThemeId[it.themeId]!
+        .preprocess(it.tileset, zoom: it.zoom.toDouble());
